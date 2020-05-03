11d3d83a0d26566bf605bf0184d44d97e3128b4d
notsmatch <smatchjp@gmail.com>
2020-05-03 17:51:50 +0900

    fix token

--- src/main/kotlin/me/notsmatch/kyoshubot/Secret.kt
+++ src/main/kotlin/me/notsmatch/kyoshubot/Secret.kt
@@ -3,5 +3,5 @@ package me.notsmatch.kyoshubot
 object Secret {
 
     @JvmStatic
-    val TOKEN: String = "NzA1NTU5NTM5ODcyNjk0Mjcy.XqtdlA.WCGcy-wlQRNxOttKv0hUPtJCHRM"
+    val TOKEN: String = "NzA1NTU5NTM5ODcyNjk0Mjcy.Xq6F9g.3dirfuIz-GPX9KNcPDr1YUIeV4w"
 }
