66101f716b3b65622326e88fb0728360f1968279
notsmatch <smatchjp@gmail.com>
2020-05-07 18:24:17 +0900

    add JsonUtils

---
+++ src/main/kotlin/me/notsmatch/kyoshubot/util/JsonUtils.kt
@@ -0,0 +1,9 @@
+package me.notsmatch.kyoshubot.util
+
+import com.google.gson.Gson
+import com.google.gson.JsonParser
+
+object JsonUtils {
+    val GSON = Gson()
+    val JSON_PARSER = JsonParser()
+}
