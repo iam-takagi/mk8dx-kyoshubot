f76c5bbaba78271cbc5720a484695d3d0a2f95e6
notsmatch <smatchjp@gmail.com>
2020-05-07 18:24:59 +0900

    add mongo covert methods

--- src/main/kotlin/me/notsmatch/kyoshubot/model/Boshu.kt
+++ src/main/kotlin/me/notsmatch/kyoshubot/model/Boshu.kt
@@ -1,12 +1,91 @@
 package me.notsmatch.kyoshubot.model
 
+import com.mongodb.BasicDBList
+import me.notsmatch.kyoshubot.Bot
+import me.notsmatch.kyoshubot.util.JsonUtils
+import org.bson.Document
+
 data class Boshu(val guildId: Long, val channelId: Long, val title: String, var koumokuList: MutableList<Koumoku>) {
 
     var messageId: Long = 0
 
+    /**
+     * @return 時間から項目を返します
+     */
     fun getKoumokuByHour(hour: Int) : Koumoku? {
         val a  = koumokuList.stream().filter { it.hour == hour }.findFirst()
         if(a.isPresent)return a.get()
         return null
+    }
+
+    /**
+     * @return ドキュメントにして返します
+     */
+    fun toDocument() : Document {
+        return Document().apply {
+            put("channelId", channelId)
+            put("guildId", guildId)
+
+            val koumokuArray = BasicDBList()
+
+            koumokuList.forEach { koumoku ->
+                koumokuArray.add(koumoku.toJsonObject().toString())
+            }
+        }
+    }
+
+    /**
+     * 保存します
+     */
+    fun save() {
+        Bot.mongoService.replaceBoshu(guildId, channelId, toDocument())
+    }
+
+    companion object {
+
+        /**
+         * @return Documentから募集オブジェクトにして返します
+         */
+
+        fun toBoshu(document: Document) : Boshu {
+            document.apply {
+
+                //JsonArrayから項目リストを作成
+                val koumokuList = mutableListOf<Koumoku>()
+                val docClazz: Class<out MutableList<*>?> = ArrayList<String>().javaClass
+                val array = get("koumoku", docClazz)!!
+
+                //項目取り出し
+                array.forEach{ str ->
+
+                    //挙手ユーザー取り出し
+                    val koumokuJson = JsonUtils.JSON_PARSER.parse(str.toString()).asJsonObject
+                    val docClazz: Class<out MutableList<*>?> = ArrayList<Long>().javaClass
+                    val kyoshuUsersArray = get("kyoshuUsers", docClazz)!!
+                    val kyoshuUsers = mutableListOf<Long>()
+
+                    //挙手ユーザーリストに追加
+                    kyoshuUsersArray.forEach{ str2 ->
+                        val kyoshuUserJson = JsonUtils.JSON_PARSER.parse(str2.toString()).asJsonObject
+                        kyoshuUsers.add(kyoshuUserJson.get("id").asLong)
+                    }
+
+                    //項目リストに追加
+                    koumokuList.add(
+                        Koumoku(
+                            koumokuJson.get("title").asString,
+                            koumokuJson.get("hour").asInt,
+                            koumokuJson.get("need").asInt,
+                            kyoshuUsers
+                        )
+                    )
+                }
+
+                //募集オブジェクトにしてreturn
+                return Boshu(
+                    document.getLong("guildId"),  document.getLong("channel"), document.getString("title"), koumokuList
+                )
+            }
+        }
     }
 }
--- src/main/kotlin/me/notsmatch/kyoshubot/model/Koumoku.kt
+++ src/main/kotlin/me/notsmatch/kyoshubot/model/Koumoku.kt
@@ -1,11 +1,34 @@
 package me.notsmatch.kyoshubot.model
 
-import net.dv8tion.jda.api.entities.User
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
 
 /**
  * @param title タイトル
- * @param time 1 ~ 24で時間を指定
+ * @param hour 1 ~ 24で時間を指定
  * @param need 募集人数
  * @param kyoshuUsers 挙手してるユーザー
  */
-data class Koumoku(val title: String, val hour: Int, val need: Int, val kyoshuUsers: MutableList<Long>)
+data class Koumoku(val title: String, val hour: Int, val need: Int, val kyoshuUsers: MutableList<Long>) {
+
+    /**
+     * @return JsonObjectにして返します
+     */
+    fun toJsonObject() : JsonObject {
+        val returnJson = JsonObject()
+        returnJson.addProperty("title", title)
+        returnJson.addProperty("hour", hour)
+        returnJson.addProperty("need", need)
+
+        val array = JsonArray()
+        kyoshuUsers.forEach{ id ->
+            val json = JsonObject()
+            json.addProperty("id", id)
+            array.add(json)
+        }
+
+        returnJson.addProperty("kyoshuUsers", array.toString())
+
+        return returnJson
+    }
+}
