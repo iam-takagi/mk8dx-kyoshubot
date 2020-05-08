d742dab33c698dc91c6457626684c9f01825a31b
notsmatch <smatchjp@gmail.com>
2020-05-08 11:16:40 +0900

    add println guilds log

--- src/main/kotlin/me/notsmatch/kyoshubot/Bot.kt
+++ src/main/kotlin/me/notsmatch/kyoshubot/Bot.kt
@@ -46,6 +46,8 @@ class Bot (private val token: String) {
 class Listener : ListenerAdapter() {
 
     override fun onReady(event: ReadyEvent) {
+        event.jda.guilds.forEach{guild -> println(guild.name)}
+
         val timer = Timer()
         timer.schedule(object : TimerTask() {
             override fun run() {
