3244802930a040bdc4ddc2901c17a1eb547d6d26
notsmatch <smatchjp@gmail.com>
2020-05-27 22:31:58 +0900

    add: GuildSettings.kt

---
+++ src/main/kotlin/me/notsmatch/kyoshubot/model/GuildSettings.kt
@@ -0,0 +1,80 @@
+package me.notsmatch.kyoshubot.model
+
+import com.google.gson.JsonObject
+import com.mongodb.BasicDBList
+import me.notsmatch.kyoshubot.Bot
+import me.notsmatch.kyoshubot.util.JsonUtils
+import net.dv8tion.jda.api.entities.Guild
+import org.bson.Document
+
+data class CommandOption(val command: String, val visibility: Boolean) {
+
+    fun toJsonObject() : JsonObject {
+        val toReturn = JsonObject()
+        toReturn.addProperty("command", command)
+        toReturn.addProperty("visibility", visibility)
+
+        return toReturn
+    }
+}
+
+data class GuildSettings(val guildId: Long, var mention: String?, val commandOptions: MutableList<CommandOption>) {
+
+    fun toDocument() : Document {
+        return Document().apply {
+            put("guildId", guildId)
+            put("mention", mention)
+            val commandOptionsArray = BasicDBList()
+
+            commandOptions.forEach { option ->
+                commandOptionsArray.add(option.toJsonObject().toString())
+            }
+
+            put("commandOptions", commandOptionsArray)
+        }
+    }
+
+    fun save() {
+        Bot.mongoService.replaceGuildSettings(guildId, toDocument())
+    }
+
+    fun getMentionString(guild: Guild) : String {
+        if(mention == null){
+            return "@everyone"
+        } else {
+            if(mention.equals("here")){
+                return "@here"
+            }
+            if(mention.equals("everyone")){
+                return "@everyone"
+            }
+            val role = guild.getRoleById(mention!!)
+            if(role == null || !role.isMentionable){
+                this.mention = "everyone"
+                return "@everyone"
+            }
+            return role.asMention
+        }
+    }
+
+    companion object {
+
+        fun fromDocument(document: Document) : GuildSettings {
+            document.apply {
+                //格納用
+                val commandOptions = mutableListOf<CommandOption>()
+
+                val docClazz: Class<out MutableList<*>?> = ArrayList<String>().javaClass
+                val commandOptionsArray = get("commandOptions", docClazz)!!
+
+                //取り出し
+                commandOptionsArray.forEach { str ->
+                    val commandOptionJson = JsonUtils.JSON_PARSER.parse(str.toString()).asJsonObject
+                    commandOptions.add(CommandOption(commandOptionJson.get("command").asString, commandOptionJson.get("visibility").asBoolean))
+                }
+
+                return GuildSettings(getLong("guildId"), getString("mention"), commandOptions)
+            }
+        }
+    }
+}
