ca7581c6b7e2644845641ecb3b9000cb4194a6db
notsmatch <smatchjp@gmail.com>
2020-05-29 14:58:40 +0900

    add: ReminderCommand 処理を追加

--- src/main/kotlin/me/notsmatch/kyoshubot/command/ReminderCommand.kt
+++ src/main/kotlin/me/notsmatch/kyoshubot/command/ReminderCommand.kt
@@ -4,12 +4,16 @@ import com.jagrosh.jdautilities.command.Command
 import com.jagrosh.jdautilities.command.CommandEvent
 import me.notsmatch.kyoshubot.service.BoshuService
 import me.notsmatch.kyoshubot.service.GuildSettingsService
+import me.notsmatch.kyoshubot.util.NumberUtils
+import net.dv8tion.jda.api.EmbedBuilder
+import org.apache.commons.lang3.StringUtils
+import java.awt.Color
 
 class ReminderCommand(val boshuService: BoshuService, val settingsService: GuildSettingsService) : Command() {
 
     init {
         this.name = "reminder"
-        this.help = "挙手してないユーザーへメンションを飛ばします"
+        this.help = "指定した時間に挙手してないユーザーへメンションを飛ばします"
         this.arguments = "<hour1> <hour2> <hour3>..."
     }
 
@@ -22,7 +26,60 @@ class ReminderCommand(val boshuService: BoshuService, val settingsService: Guild
                 event.message.delete().complete()
             }
 
+            val boshu = boshuService.getBoshu(guild.idLong, channel.idLong)
+                ?: return replyInDm(EmbedBuilder().apply {
+                    setColor(Color.RED)
+                    setAuthor(
+                        "Error",
+                        null,
+                        null
+                    )
+                    setDescription("このチャンネルでは募集が開始されていません。")
+                }.build())
 
+            val args = StringUtils.split(args)
+
+            if (args.isNotEmpty()) {
+
+                args.forEach { arg ->
+                    if (!NumberUtils.isInteger(arg) || arg.toInt() > 36 || arg.toInt() < 0) {
+                        return replyInDm(EmbedBuilder().apply {
+                            setColor(Color.RED)
+                            setAuthor(
+                                "Error",
+                                null,
+                                null
+                            )
+                            setDescription("hourは0~36で指定する必要があります。")
+                        }.build())
+                    }
+
+                    val koumoku = boshu.getKoumokuByHour(arg.toInt()) ?: return replyInDm(EmbedBuilder().apply {
+                        setColor(Color.RED)
+                        setAuthor(
+                            "Error",
+                            null,
+                            null
+                        )
+                        setDescription("${arg}時の項目は存在しません")
+                    }.build())
+
+                    if (koumoku.isClosed()) {
+                        return replyInDm(EmbedBuilder().apply {
+                            setColor(Color.RED)
+                            setAuthor(
+                                "Error",
+                                null,
+                                null
+                            )
+                            setDescription("${arg}時の項目は締め切られています")
+                        }.build())
+                    }
+
+                    val channel = boshu.channelId.let { guild.getTextChannelById(it) } ?:return
+                    channel.sendMessage("${arg}時: @everyone @" + koumoku.getKyoshuLeft()).queue()
+                }
+            }
         }
     }
 }
