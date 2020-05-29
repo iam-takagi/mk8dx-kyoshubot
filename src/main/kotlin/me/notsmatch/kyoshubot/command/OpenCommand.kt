4329fe35aa7f8e651840624a8dc3f862cd6647ea
notsmatch <smatchjp@gmail.com>
2020-05-29 14:45:58 +0900

    fix: OpenCommand 処理追加

--- src/main/kotlin/me/notsmatch/kyoshubot/command/OpenCommand.kt
+++ src/main/kotlin/me/notsmatch/kyoshubot/command/OpenCommand.kt
@@ -4,11 +4,15 @@ import com.jagrosh.jdautilities.command.Command
 import com.jagrosh.jdautilities.command.CommandEvent
 import me.notsmatch.kyoshubot.service.BoshuService
 import me.notsmatch.kyoshubot.service.GuildSettingsService
+import me.notsmatch.kyoshubot.util.NumberUtils
+import net.dv8tion.jda.api.EmbedBuilder
+import org.apache.commons.lang3.StringUtils
+import java.awt.Color
 
 class OpenCommand(val boshuService: BoshuService, val settingsService: GuildSettingsService) : Command() {
 
     init {
-        this.name = "reopen"
+        this.name = "open"
         this.help = "強制締め切りした項目を解除します"
         this.arguments = "<hour1> <hour2> <hour3>..."
     }
@@ -18,11 +22,66 @@ class OpenCommand(val boshuService: BoshuService, val settingsService: GuildSett
 
             val settings = settingsService.getGuildSettings(guild.idLong)
 
-            if (settings.getCommandOption("reopen") == null || !settings.getCommandOption("reopen")!!.visibility) {
+            if (settings.getCommandOption("open") == null || !settings.getCommandOption("open")!!.visibility) {
                 event.message.delete().complete()
             }
 
+            val boshu = boshuService.getBoshu(guild.idLong, channel.idLong)
+                ?: return replyInDm(EmbedBuilder().apply {
+                    setColor(Color.RED)
+                    setAuthor(
+                        "Error",
+                        null,
+                        null
+                    )
+                    setDescription("このチャンネルでは募集が開始されていません。")
+                }.build())
 
+            val args = StringUtils.split(args)
+
+            if (args.isNotEmpty()) {
+
+                args.forEach { arg ->
+                    if (!NumberUtils.isInteger(arg) || arg.toInt() > 36 || arg.toInt() < 0) {
+                        return replyInDm(EmbedBuilder().apply {
+                            setColor(Color.RED)
+                            setAuthor(
+                                "Error",
+                                null,
+                                null
+                            )
+                            setDescription("hourは0~36で指定する必要があります。")
+                        }.build())
+                    }
+
+                    val koumoku = boshu.getKoumokuByHour(arg.toInt()) ?: return replyInDm(EmbedBuilder().apply {
+                        setColor(Color.RED)
+                        setAuthor(
+                            "Error",
+                            null,
+                            null
+                        )
+                        setDescription("${arg}時の項目は存在しません")
+                    }.build())
+
+                    //強制締めきりされてるとき
+                    if (koumoku.closed) {
+                        koumoku.closed = false
+                        boshu.save()
+                        boshu.updateMessage(guild, settings)
+                    } else {
+                        return replyInDm(EmbedBuilder().apply {
+                            setColor(Color.RED)
+                            setAuthor(
+                                "Error",
+                                null,
+                                null
+                            )
+                            setDescription("${arg}時の項目は締め切りされていません")
+                        }.build())
+                    }
+                }
+            }
         }
     }
 }
