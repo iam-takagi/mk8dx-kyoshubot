68180151c9c90263a0e690e21c8a95ac4b7bbb4c
notsmatch <smatchjp@gmail.com>
2020-05-29 14:42:18 +0900

    add: ReopenCommand.kt

---
+++ src/main/kotlin/me/notsmatch/kyoshubot/command/ReopenCommand.kt
@@ -0,0 +1,28 @@
+package me.notsmatch.kyoshubot.command
+
+import com.jagrosh.jdautilities.command.Command
+import com.jagrosh.jdautilities.command.CommandEvent
+import me.notsmatch.kyoshubot.service.BoshuService
+import me.notsmatch.kyoshubot.service.GuildSettingsService
+
+class ReopenCommand(val boshuService: BoshuService, val settingsService: GuildSettingsService) : Command() {
+
+    init {
+        this.name = "reopen"
+        this.help = "強制締め切りした項目を解除します"
+        this.arguments = "<hour1> <hour2> <hour3>..."
+    }
+
+    override fun execute(event: CommandEvent?) {
+        event?.apply {
+
+            val settings = settingsService.getGuildSettings(guild.idLong)
+
+            if (settings.getCommandOption("reopen") == null || !settings.getCommandOption("reopen")!!.visibility) {
+                event.message.delete().complete()
+            }
+
+            
+        }
+    }
+}
